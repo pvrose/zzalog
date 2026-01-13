6e45a436acd205a2df05857824574749944a6648
Philip Rose <pvrose@btinternet.com>
2026-01-13 21:15:08 +0000

    Move ident back to zzalog

---
+++ include/ident.h
@@ -0,0 +1,21 @@
+#include <string>
+
+//! Program timestamp - set by CMake
+extern std::string APP_TIMESTAMP;
+//! Progrsm version - set by CMake
+extern std::string APP_VERSION;
+//! Program name - set by CMake
+extern std::string APP_NAME;
+//! Program vendor - set by CMake
+extern std::string APP_VENDOR;
+//! Program copyright - displayed in all windows.
+extern std::string COPYRIGHT;
+//! Third-party acknowledgments.
+extern std::string PARTY3RD_COPYRIGHT;
+//! Contact address for use in FLTK widget labels.
+extern std::string CONTACT;
+//! Contact address for use in general texts.
+extern std::string CONTACT2;
+//! Copyright placed in exported data items.
+extern std::string DATA_COPYRIGHT;
+
