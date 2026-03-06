7a10a042f8a4d30d8cd4ee097249bade52ce89f9
pvrose <gm3zza@btinternet.com>
2026-03-06 14:28:02 +0000

    Cold start - insist on either town/city or 6-character locator.

--- src/stn_data.cpp
+++ src/stn_data.cpp
@@ -129,35 +129,38 @@ void stn_data::load_data() {
 		current_.name = previous_oper_;
 	}
 	// If no data loaded or default callsign is not set
-	if (!loaded || defaults_.callsign.length() == 0) {
+	while (!loaded || defaults_.callsign.length() == 0) {
 		// Create a set of initial values from input defaults.
 		stn_window_->set_tab(stn_dialog::DEFAULTS, "", "No station data loaded, set default values.");
 		while (stn_window_->visible()) Fl::check();
+		loaded = true;
 	}
-	if (qths_.find(defaults_.location) == qths_.end()) {
+	while (qths_.find(defaults_.location) == qths_.end()) {
 		// Create a set of initial values from input defaults.
 		stn_window_->set_tab(stn_dialog::QTH, defaults_.location, "QTH not known, please enter details.");
 		while (stn_window_->visible()) Fl::check();
 	}
 	auto qth = qths_.at(defaults_.location);
-	if (qth->data.find(qth_value_t::CITY) == qth->data.end() ||
-        qth->data.find(qth_value_t::LOCATOR) == qth->data.end()) {
+	while (qth->data.find(qth_value_t::CITY) == qth->data.end() &&
+        (qth->data.find(qth_value_t::LOCATOR) == qth->data.end() ||
+			qth->data.at(qth_value_t::LOCATOR).length() < 6)) {
 		// Create a set of initial values from input defaults.
 		stn_window_->set_tab(stn_dialog::QTH, defaults_.location, "QTH not known, please enter details.");
 		while (stn_window_->visible()) Fl::check();
 	}
-	if (defaults_.type == INDIVIDUAL &&
-		opers_.find(defaults_.name) == opers_.end()) {
-		// Create a set of initial values from input defaults.
-		stn_window_->set_tab(stn_dialog::OPERATOR, defaults_.name, "Operator not known, please enter details.");
-		while (stn_window_->visible()) Fl::check();
+	if (defaults_.type == INDIVIDUAL) {
+		while (opers_.find(defaults_.name) == opers_.end()) {
+			// Create a set of initial values from input defaults.
+			stn_window_->set_tab(stn_dialog::OPERATOR, defaults_.name, "Operator not known, please enter details.");
+			while (stn_window_->visible()) Fl::check();
+		}
 	}
 	if (defaults_.type == CLUB) {
 		// Create a set of initial values from input defaults.
 		stn_window_->set_tab(stn_dialog::OPERATOR, previous_oper_, "Club station, please specify operator");
 		while (stn_window_->visible()) Fl::check();
 	}
-	if (calls_.find(defaults_.callsign) == calls_.end()) {
+	while (calls_.find(defaults_.callsign) == calls_.end()) {
 		// Create a set of initial values from input defaults.
 		stn_window_->set_tab(stn_dialog::CALLSIGN, defaults_.callsign, "Station callsign not known, please enter details.");
 		while (stn_window_->visible()) Fl::check();
