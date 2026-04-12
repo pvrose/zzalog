ab4a2a265ab1127f14e8ea31dcfd355eb42f3ac0
pvrose <gm3zza@btinternet.com>
2026-04-12 21:43:51 +0100

    Updated zzacommon

--- include/contest_algorithm.h
+++ include/contest_algorithm.h
@@ -14,7 +14,8 @@
     You should have received a copy of the GNU General Public License along with ZZALOG. 
 	If not, see <https://www.gnu.org/licenses/>. 
 
-*/#pragma once
+*/
+#pragma once
 
 #include <map>
 #include <set>
